bcea5bd4c3e42cdcd4f00a0531fdf83bbe3067b6
Jeremy <166249059+JeremySu0818@users.noreply.github.com>
2025-06-09 00:32:06 +0800

    Delete 轉ico.py

--- 轉ico.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-
-img = Image.open("icon.png")
-img = img.convert("RGBA")  # 確保含透明度
-sizes = [(16, 16), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)]
-img.save("icon.ico", format="ICO", sizes=sizes)
